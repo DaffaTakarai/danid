d91e0b5f5844ff7e5c734760056c0cf8997f55f2
DaffaTakarai <gacktioto@gmail.com>
2018-09-29 22:56:52 +0700

    Initial commit

---
+++ src/denganid.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+	IDSaya()
+}
+fun IDSaya(){
+	val nama = "DaffaTakarai"
+	val code1 =  16668813863
+	val code2 = 71731363637
+	println("Nama Saya Adalah : $nama")
+	print("Nomer Id Saya : ${code1+code2}")
+}
